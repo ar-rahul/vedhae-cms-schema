815cf199e15cd1fd6e921e6d7c8b683d43e794fb
Rahul <2ar.rahul@gmail.com>
2026-02-01 12:31:35 +0530

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,5 @@ export { CardSectionSchema } from "./cardSection.schema.js";
 // Products
 export { ProductSchema } from "./product.schema.js";
 export { ProductHeroSchema } from "./productHero.schema.js";
-// Product meta (ROUTES / CATEGORIES / USERS)
-export { ProductCategorySchema, ProductUserSchema, ProductRouteSchema, } from "./product.meta.js";
 // About
 export { AboutHeroSchema, AboutContentSchema, AboutImagesSchema, } from "./about.schema.js";
